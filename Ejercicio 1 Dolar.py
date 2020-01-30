7dadbe8260e982b1f7b9cf77a9affec31b04a1a2
guanarp <60052556+guanarp@users.noreply.github.com>
2020-01-30 18:06:41 -0300

    Add files via upload

---
+++ Ejercicio 1 Dolar.py
@@ -0,0 +1,32 @@
+"""Crear desde la línea de comandos un sistema donde podrás 
+ingresar el monto que deseas cambiar, el sistema debe de solicitar 
+al usuario desde la terminal que ingrese el monto en USD 
+y la salida debe ser el monto cambiado a PYG. También puedes hacer otro programa que convierta de PYG a USD."""
+
+VentaDolar = 6350
+CompraDolar = 6310 #Factores de conversion
+verificador = True #booleano para que funcione el programa
+#Por comodidad hice los dos programas en un codigo
+
+while(verificador == True ):
+    key = input("Digite 0 si desea cambiar de USD a PYG, caso contrario digite 1 \n") #El usuario decide que tipo de conversion quiere hacer
+    if (key.isnumeric() == True and int(key) == 0): #de USD a PYG con validacion de que se ingresa un numero
+        dinero = input("Ingrese el monto en USD a cambiar: \n") #Monto a cambiar
+        if (dinero.isnumeric() == True): #validacion de que se ingresa un int
+            resultado = int(dinero) * CompraDolar #Convertido
+            print("El monto equivale a", resultado, "PYG, que tenga un lindo dia") #Resultado en terminal
+            verificador = False #se termina el programa ya que se realizo la conversion
+        else: #mensaje de error
+            print('No se ha ingresado un monto correcto, caracter invalido')    
+
+    elif (key.isnumeric() == True and int(key) == 1): #de PYG a USD con validacion de que se ingresa un numero
+        dinero = input("Ingrese el monto en PYG a cambiar \n") #Monto a cambiar
+        if (dinero.isnumeric() == True): #validacion de que se ingresa int
+            resultado = int(dinero) / VentaDolar #convertido
+            print( "El monto equivale a ", resultado, "USD, que tenga un lindo dia") #resultado
+            verificador = False #se termina el programa ya que se realizo la conversion
+        else:
+            print('No se ha ingresado un monto correcto, caracter invalido') #mensaje de error   
+
+    else:
+        print("Opcion incorrecta debe ingresar 0 o 1 \n") #mensaje de error           
